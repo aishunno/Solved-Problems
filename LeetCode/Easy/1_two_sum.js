2df4044a6624efb4d19306b1eedcc6fe9480f7d2
arifulIslamSelise <ariful.islam@selise.ch>
2021-10-22 12:39:13 +0600

    Two sum solved in js

---
+++ LeetCode/Easy/1_two_sum.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    const visited = new Map();
+
+    for(let i = 0; i < nums.length; i++) {
+        const req_num = target - nums[i];
+        
+        if (visited.has(req_num)) {
+            console.log(visited.get(req_num))
+            return [i, visited.get(req_num)];
+        }
+
+        visited.set(nums[i], i);
+    }
+
+    return [];
+};
+
+// console.log(twoSum([2, 7, 11, 15], 18));
