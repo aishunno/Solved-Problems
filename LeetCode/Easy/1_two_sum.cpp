d667a46ddc69eba0f78b2f473ad1e327f98a405d
arifulIslamSelise <ariful.islam@selise.ch>
2021-10-22 11:54:50 +0600

    Two sum problem solved

---
+++ LeetCode/Easy/1_two_sum.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+// Brute-force solution
+// vector<int> twoSum(vector<int>& nums, int target) {
+//     for (int i = 0; i < nums.size(); i++)
+//     {
+//         for (int j = i + 1; j < nums.size(); j++)
+//         {
+//             if (nums[i] + nums[j] == target)
+//             {
+//                 return {i, j};
+//             }
+            
+//         }
+//     }
+
+//     return {};
+// }
+
+class Solution {
+public:
+    static vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> visited;
+        for (int i = 0; i < nums.size(); i ++) {
+            int required_number = target - nums[i];
+
+            if (visited.count(required_number) > 0) {
+                return {visited.at(required_number), i};
+            } else {
+                visited[nums[i]] = i;
+            }
+        }
+        return {};
+    }
+};
+
+vector <int> twoSum(vector<int>& nums, int target) {
+    unordered_map<int, int> visited;
+    for (int i = 0; i < nums.size(); i ++) {
+        int required_number = target - nums[i];
+        
+        if (visited.count(required_number) > 0) {
+            return {visited.at(required_number), i};
+        } else {
+            visited[nums[i]] = i;
+        }
+    }
+    return {};
+}
+
+int main(void) {
+    vector <int> nums = {3, 3};
+    int target = 6;
+    vector<int> result = Solution::twoSum(nums, target);
+    for (auto it: result) {
+        cout << it << " ";
+    }
+    return 0;
+}
