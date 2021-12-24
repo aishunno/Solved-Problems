ec63362ed2c3f98a47d8c7cd9c1d428fd49dc4e2
arifulIslamSelise <ariful.islam@selise.ch>
2021-12-24 21:07:06 +0600

    121 solution using python

---
+++ LeetCode/Easy/121.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        maxProfit = 0
+
+        buyPtr = 0
+        sellPtr = 1
+
+        while sellPtr < len(prices):
+            if prices[buyPtr] < prices [sellPtr]:
+                profit = prices[sellPtr] - prices[buyPtr]
+                maxProfit = max(maxProfit, profit)
+            else:
+                buyPtr = sellPtr
+
+            sellPtr += 1
+
+            
+        return maxProfit
