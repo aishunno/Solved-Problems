4dee084ffc38a78e61e554f68055c2aa07e6c5da
arifulIslamSelise <ariful.islam@selise.ch>
2022-01-22 10:29:26 +0600

    Solution of problem 26

--- LeetCode/Easy/26.py
+++ LeetCode/Easy/26.py
@@ -1,12 +1,12 @@
-a = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
-
-leftPtr = 1
-rightPtr = 1
-
-for i in range(1, len(a)):
-    if a[i] != a[i - 1]:
-        a[leftPtr] = a[i]
-        leftPtr += 1
+from typing import List
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        leftPtr = 1
         
-
-print (a, leftPtr)
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i - 1]:
+                nums[leftPtr] = nums[i]
+                leftPtr += 1
+                
+        return leftPtr
+        
